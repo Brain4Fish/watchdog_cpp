40cfcf6ed2a5700dcdd188716514a40a05c64b91
Mikhail Kozlov <Coffe-man@mail.ru>
2015-07-24 14:35:15 +0600

    8

--- watchdog_cpp/watchdog.cpp
+++ watchdog_cpp/watchdog.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 int main() {
-	if (!process_check.compare(system("ps afx | grep 'dpi-engine | grep -v grep"))) {
+	if (!process_check.compare(system(ps afx | grep "dpi-engine" | grep -v grep))) {
 		printf("dont find dpi process\n");
 	}
 	else {
