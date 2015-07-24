43aa9e2888f684f4591f1b0d6e04ce6a7ecf21cd
Mikhail Kozlov <Coffe-man@mail.ru>
2015-07-24 14:23:13 +0600

    4

--- watchdog_cpp/watchdog.cpp
+++ watchdog_cpp/watchdog.cpp
@@ -2,17 +2,18 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstdio>
+#include "watchdog.h"
 
 using namespace std;
 
 int main() {
-	char process_check;
-	char proc = system("ps afx | grep "dpi-engine" | grep -v grep");
-	if (!process_check.compare(proc) {
-		cout << "dont find dpi process" << endl;
+	char proc = system("ps afx | grep 'dpi-engine | grep -v grep");
+	if (!process_check.compare(proc)) {
+		printf("dont find dpi process\n");
 	}
 	else {
-		cout << "find dpi process" << endl;
+		printf("find dpi process\n");
 	}
 	return 0;
 }
