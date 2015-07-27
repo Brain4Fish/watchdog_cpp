8517a9c92264ffe62d987088b963484e67fbfe89
Mikhail Kozlov <Coffe-man@mail.ru>
2015-07-27 16:04:44 +0600

    13

--- watchdog_cpp/watchdog.h
+++ watchdog_cpp/watchdog.h
@@ -1,2 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
 int main();
-/*char process_check[] = "ps afx | grep "dpi-engine" | grep -v grep"
+
+bool processCheck(void) {
+	if (system("ps afx | grep 'dpi-engine' | grep -v grep") == true) {
+		printf("Dpi process found\n");
+		return 0;
+	}
+	else {
+		printf("Dpi process not found\n");
+		return 1;
+	}
+}
+
