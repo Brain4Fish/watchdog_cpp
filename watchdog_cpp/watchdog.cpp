b3b839e3806763ad42bb0ac3581d88323f99f6f4
Mikhail Kozlov <Coffe-man@mail.ru>
2015-07-27 15:48:10 +0600

    11

--- watchdog_cpp/watchdog.cpp
+++ watchdog_cpp/watchdog.cpp
@@ -1,17 +1,62 @@
+#include <sys/types.h>
+#include <errno.h>
+#include <vector>
+#include <string>
 #include <iostream>
-#include <string.h>
-#include <stdio.h>
+#include <dirent.h>
+#include <fstream>
 #include <stdlib.h>
-#include <cstdio>
+#include <stdio.h>
 #include "watchdog.h"
 
 using namespace std;
 
-int main() {
-	
-	if (isRunning("dpi-engine") == 0) {
-		printf("Dpi process found\n");
+int getProcIdByName(string procName)
+{
+	int pid = -1;
+
+	// Open the /proc directory
+	DIR *dp = opendir("/proc");
+	if (dp != NULL)
+	{
+		// Enumerate all entries in directory until process found
+		struct dirent *dirp;
+		while (pid < 0 && (dirp = readdir(dp)))
+		{
+			// Skip non-numeric entries
+			int id = atoi(dirp->d_name);
+			if (id > 0)
+			{
+				// Read contents of virtual /proc/{pid}/cmdline file
+				string cmdPath = string("/proc/") + dirp->d_name + "/cmdline";
+				ifstream cmdFile(cmdPath.c_str());
+				string cmdLine;
+				getline(cmdFile, cmdLine);
+				if (!cmdLine.empty())
+				{
+					// Keep first cmdline item which contains the program path
+					size_t pos = cmdLine.find('\0');
+					if (pos != string::npos)
+						cmdLine = cmdLine.substr(0, pos);
+					// Keep program name only, removing the path
+					pos = cmdLine.rfind('/');
+					if (pos != string::npos)
+						cmdLine = cmdLine.substr(pos + 1);
+					// Compare against requested process name
+					if (procName == cmdLine)
+						pid = id;
+				}
+			}
+		}
 	}
-	else printf("Dpi process not found\n");
+	closedir(dp);
+	return pid;
+}
+
+int main(int argc, char* argv[])
+{
+	// Fancy command line processing skipped for brevity
+	int pid = getProcIdByName(argv[1]);
+	cout << "pid: " << pid << endl;
 	return 0;
 }
--- watchdog_cpp/watchdog.h
+++ watchdog_cpp/watchdog.h
@@ -1,9 +1,2 @@
 int main();
-/*char process_check[] = "ps afx | grep "dpi-engine" | grep -v grep";
-*/
-bool isRunning(const char* name)
-{
-	char command[32];
-	sprintf(command, "pgrep %s > /dev/null", name);
-	return 0 == system(command);
-}
+/*char process_check[] = "ps afx | grep "dpi-engine" | grep -v grep"
